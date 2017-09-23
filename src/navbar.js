1b46d4d755a94bec1182e64158df98fc711ae687
nkernis <nkernis@gmail.com>
2017-09-23 15:37:08 -0400

    new navbar

--- src/navbar.js
+++ src/navbar.js
@@ -5,12 +5,19 @@ class NavBar {
 
   template () {
     return `
-      <ul id="nav-ul">
-        <li><a href='index.html'><h3>POWER LUNCH</h3></a></li>
-        <li><a href='episodes.html'><h3>EPISODES</h3></a></li>
-        <li><a href='people.html'><h3>PEOPLE</h3></a></li>
-        <li><a href='contact.html'><h3>CONTACT</h3></a></li>
-      </ul>
+      <nav class="navbar navbar-expand-sm navbar-light bg-light navbar-center">
+        <a class="navbar-brand" href='index.html'><h3>POWER LUNCH</h3></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link" href='episodes.html'><h3>EPISODES</h3></a>
+            <a class="nav-item nav-link" href='people.html'><h3>PEOPLE</h3></a>
+            <a class="nav-item nav-link" href='contact.html'><h3>CONTACT</h3></a>
+          </div>
+        </div>
+      </nav>
     `
   }
 
@@ -21,3 +28,23 @@ class NavBar {
 
 const app = new NavBar()
 app.render()
+
+//
+// <nav class="navbar navbar-expand-sm ">
+//   <div class="collapse navbar-collapse">
+//       <ul class="navbar-nav mr-auto">
+//         <li class="nav-item">
+//           <a class="nav-link" href='index.html'><h3>POWER LUNCH</h3></a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link" href='episodes.html'><h3>EPISODES</h3></a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link" href='people.html'><h3>PEOPLE</h3></a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link" href='contact.html'><h3>CONTACT</h3></a>
+//         </li>
+//       </ul>
+//     </div>
+//   </nav>
