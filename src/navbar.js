f0f99694f2a74d6b22f0516c276ac9be87b4752e
nkernis <nkernis@gmail.com>
2017-09-25 12:00:48 -0400

    episodes centered

--- src/navbar.js
+++ src/navbar.js
@@ -7,10 +7,10 @@ class NavBar {
     return `
       <nav class="navbar navbar-toggleable-sm">
         <div class="nav navbar-nav align-items-center">
-          <a class="nav-link" href="index.html"><h1>POWER LUNCH</h1></a>
-          <a class="nav-link" href="episodes.html"><h1>EPISODES</h1></a>
-          <a class="nav-link" href="people.html"><h1>PEOPLE</h1></a>
-          <a class="nav-link" href="contact.html"><h1>CONTACT</h1></a>
+          <a class="nav-link-x" href="index.html"><h1>POWER LUNCH</h1></a>
+          <a class="nav-link-x" href="episodes.html"><h1>EPISODES</h1></a>
+          <a class="nav-link-x" href="people.html"><h1>PEOPLE</h1></a>
+          <a class="nav-link-x" href="contact.html"><h1>CONTACT</h1></a>
         </div>
       </nav>
     `
