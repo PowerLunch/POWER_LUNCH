42580d7e08fa4698bfac49c89f4bd68208fb6f2d
nkernis <nkernis@gmail.com>
2017-09-25 15:00:17 -0400

    people changed to team

--- src/navbar.js
+++ src/navbar.js
@@ -10,7 +10,7 @@ class NavBar {
           <a class="nav-link-x" href="index.html"><h1>POWER LUNCH</h1></a>
           <a class="nav-link-x" href="about.html"><h1>ABOUT</h1></a>
           <a class="nav-link-x" href="episodes.html"><h1>EPISODES</h1></a>
-          <a class="nav-link-x" href="people.html"><h1>PEOPLE</h1></a>
+          <a class="nav-link-x" href="team.html"><h1>TEAM</h1></a>
           <a class="nav-link-x" href="contact.html"><h1>CONTACT</h1></a>
         </div>
       </nav>
