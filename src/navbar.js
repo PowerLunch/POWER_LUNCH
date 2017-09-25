e61bfa5dbb9edd83352bd30b1a34b3f0cf94c720
nkernis <nkernis@gmail.com>
2017-09-25 14:42:32 -0400

    about page

--- src/navbar.js
+++ src/navbar.js
@@ -8,6 +8,7 @@ class NavBar {
       <nav class="navbar navbar-toggleable-sm">
         <div class="nav navbar-nav align-items-center">
           <a class="nav-link-x" href="index.html"><h1>POWER LUNCH</h1></a>
+          <a class="nav-link-x" href="about.html"><h1>ABOUT</h1></a>
           <a class="nav-link-x" href="episodes.html"><h1>EPISODES</h1></a>
           <a class="nav-link-x" href="people.html"><h1>PEOPLE</h1></a>
           <a class="nav-link-x" href="contact.html"><h1>CONTACT</h1></a>
