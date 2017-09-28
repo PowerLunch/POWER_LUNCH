399d2569f921d475984d8360e489cd205280bbad
nkernis <nkernis@gmail.com>
2017-09-28 19:15:24 -0400

    fixed navbaar sizing

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+class NavBar {
+  render () {
+    return `
+      <nav class="navbar navbar-toggleable-sm">
+        <div class="nav navbar-nav align-items-center">
+          <a class="nav-link-x" href="/">POWER LUNCH</a>
+          <a class="nav-link-x" href="/about">ABOUT</a>
+          <a class="nav-link-x" href="/episodes">EPISODES</a>
+          <a class="nav-link-x" href="/team">TEAM</a>
+          <a class="nav-link-x" href="/contact">CONTACT</a>
+        </div>
+      </nav>
+    `
+  }
+}
